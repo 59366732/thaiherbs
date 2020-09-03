7797f842632d2c4a32771a04be598edecd8c8dc2
Ing Saeyang <ings59@email.nu.ac.th>
2020-09-04 00:13:53 +0700

    Add antd

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,10 @@
 // @ts-nocheck
 import React from "react";
-import example from "../assets/images_for_home_page.png";
+//import example from "../assets/images_for_home_page.png";
 const Home = () => {
   return (
     <div style={{ textAlign: "center"}}>
-      <h1>แสดงข้อมูลสมุนไพรใหม่ (pendding) มั้ย? หรือว่าไงคิดมะออก</h1>
-      <img src={example} alt="" />
+      
     </div>
   );
 }
--- src/components/QandA.jsx
+++ src/components/QandA.jsx
@@ -1,13 +1,21 @@
 // @ts-nocheck
-import React from "react";
-import example from "../assets/images_for_qanda_page.png";
+import React, { useEffect, useState } from "react";
+//import example from "../assets/images_for_qanda_page.png";
+import { useCombobox } from "downshift";
+import { Input } from "antd";
 
 const QandA = () => {
-  return (
-    <div style={{ textAlign: "center"}}>
-      <h1>ประมาณนี้ (์Now it is only image)</h1>
-      <img src={example} alt="" />
-    </div>
-  );
+  const [inputItems, setInputItems] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [single, setSingleUsers] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  console.log(users);
+  return <div style={{ textAlign: "center" }}></div>;
 };
 export default QandA;
