f609e59929c86559028a240970e954ab8c4a5323
Ing Saeyang <ings59@email.nu.ac.th>
2020-09-15 22:54:53 +0700

    Add Home UI

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,216 @@
 // @ts-nocheck
+/* cSpell:disable */
 import React from "react";
-//import example from "../assets/images_for_home_page.png";
+import "./Home.css";
 const Home = () => {
-  return (
-    <div style={{ textAlign: "center"}}>
-      
-    </div>
-  );
-}
+	return (
+		<div data-layer="e848bf72-eb2b-4af9-a0e2-535798e019a3" className="home">
+			<div
+				data-layer="a95aa301-e052-44bf-a344-34569b66cd41"
+				className="blockOne"
+			>
+				{" "}
+				<div
+					data-layer="2baac8f2-b09b-4c2f-84cd-d075fe9937d7"
+					className="nopathCopy2"
+				></div>
+				<div
+					data-layer="ecb4160c-74eb-4165-93b3-09b90ace320f"
+					className="text1"
+				>
+					ลูกใต้ใบ
+				</div>
+				<div
+					data-layer="6ae1ed2d-1cd9-45c8-8480-8861684e17f3"
+					className="time1"
+				>
+					เพิ่มเมื่อวันที่ 28/06/2563 <br />
+					เวลา 10.53 น.
+				</div>
+			</div>
+			<div
+				data-layer="004adb1d-0b08-4e14-99c7-6a388804d48f"
+				className="blockFive"
+			>
+				{" "}
+				<div
+					data-layer="4b8920ab-ce4c-42da-b5e4-fc97b188dabc"
+					className="rectangle5"
+				></div>
+				<div
+					data-layer="a6e1a351-88c3-4820-9752-2619b2e8b524"
+					className="text5"
+				>
+					ดอกอัญชัน
+				</div>
+				<div
+					data-layer="a87a9b53-c011-428c-bfbf-4c7e3ef34599"
+					className="time5"
+				>
+					เพิ่มเมื่อวันที่ 24/06/2563 <br />
+					เวลา 10.46 น.
+				</div>
+			</div>
+			<div
+				data-layer="46903ace-8e35-4c43-8728-7f7dfcc79c60"
+				className="blockTwo"
+			>
+				{" "}
+				<div
+					data-layer="37bb5f6b-07a8-4be0-b6e4-1ae3c8843322"
+					className="rectangle2"
+				></div>
+				<div
+					data-layer="f2d016a4-36fb-40ed-9dbe-fb5502a1e331"
+					className="time2"
+				>
+					แก้ไขเมื่อวันที่ 28/06/2563 <br />
+					เวลา 10.52 น.
+				</div>
+				<div
+					data-layer="e73b6f30-0b69-450b-9575-5a15a32c257a"
+					className="text2"
+				>
+					ปอกะบิด
+				</div>
+			</div>
+			<div
+				data-layer="9d39b23e-1ce8-4999-9645-3b98057fbcf9"
+				className="blockSix"
+			>
+				{" "}
+				<div
+					data-layer="59ea0d6f-ce85-424e-8ee5-83bf6e291e94"
+					className="rectangle6"
+				></div>
+				<div
+					data-layer="9d047526-c80e-429a-a0b6-885743588c80"
+					className="time6"
+				>
+					เพิ่มเมื่อวันที่ 23/06/2563 <br />
+					เวลา 15.30 น.
+				</div>
+				<div
+					data-layer="810fa85d-4c4f-4353-ae17-7aa9eef72175"
+					className="text6"
+				>
+					ตะไคร้
+				</div>
+			</div>
+			<div
+				data-layer="ffef86a9-c482-44fb-9912-a4833a745657"
+				className="blockThree"
+			>
+				{" "}
+				<div
+					data-layer="dd7d2c82-fd5c-444d-bf7f-54c4cc4a6543"
+					className="rectangle3"
+				></div>
+				<div
+					data-layer="6ab54b9b-4308-4729-897b-946494657650"
+					className="time3"
+				>
+					เพิ่มเมื่อวันที่ 28/06/2563 <br />
+					เวลา 10.51 น.
+				</div>
+				<div
+					data-layer="a00bc589-43f5-41c6-8851-c8003959ae51"
+					className="text3"
+				>
+					พลูคาว
+				</div>
+			</div>
+			<div
+				data-layer="eba738f6-2799-4e68-9bc3-d01ff556cccc"
+				className="blockSeven"
+			>
+				{" "}
+				<div
+					data-layer="bd3546e3-0fe6-4266-a8d8-23b1f920c1a2"
+					className="rectangle7"
+				></div>
+				<div
+					data-layer="7b5e5194-a445-4b71-aea2-069fa0a61755"
+					className="time7"
+				>
+					เพิ่มเมื่อวันที่ 22/06/2563 <br />
+					เวลา 15.31 น.
+				</div>
+				<div
+					data-layer="ae774d90-0993-4537-947e-747d8ffb24bb"
+					className="text7"
+				>
+					กระแตไต่ไม้
+				</div>
+			</div>
+			<div
+				data-layer="5c2864b9-eda9-4874-ab7d-ba1370bec516"
+				className="blockFour"
+			>
+				{" "}
+				<div
+					data-layer="bec6586a-096d-4645-ade1-5f5611efbfa9"
+					className="rectangle4"
+				></div>
+				<div
+					data-layer="e9e57fb0-bfa5-4ab8-985d-e402dd9fc958"
+					className="time4"
+				>
+					เพิ่มเมื่อวันที่ 28/06/2563 <br />
+					เวลา 10.50 น.
+				</div>
+				<div
+					data-layer="71db0341-75f7-475d-9ba7-ed6ca35b1b7c"
+					className="text4"
+				>
+					ปลาไหลเผือก
+				</div>
+			</div>
+			<div
+				data-layer="21615cb5-4757-4336-b4a7-e5d47861945a"
+				className="blockEight"
+			>
+				{" "}
+				<div
+					data-layer="fc1fb8fd-4076-446f-b164-9d1846101fdb"
+					className="rectangle8"
+				></div>
+				<div
+					data-layer="eaa0d6d8-ba3d-45dd-b228-f624918801c8"
+					className="time8"
+				>
+					เพิ่มเมื่อวันที่ 21/06/2563 <br />
+					เวลา 14.30 น.
+				</div>
+				<div
+					data-layer="d12f7306-8601-4291-80e7-1d375228f1e6"
+					className="text8"
+				>
+					กระชายดำ
+				</div>
+			</div>
+			<div data-layer="1bbe7eff-70ae-4c26-b55f-4281acd3a17b" className="page">
+				{" "}
+				<div
+					data-layer="ee2b46e1-3198-4e6b-ac03-39cb54fdf500"
+					className="page02"
+				></div>
+				<div data-layer="080e142a-a8fa-40a3-912e-b84acec701f0" className="x2">
+					2
+				</div>
+				<div
+					data-layer="9ec414ee-6e9d-47c5-bbf2-25462cb8d618"
+					className="page01"
+				></div>
+				<div data-layer="36f9e160-f536-4278-bf57-a2dbb7cd0172" className="x1">
+					1
+				</div>
+			</div>
+		</div>
+	);
+};
+
+Home.propTypes = {};
+
+Home.defaultProps = {};
 export default Home;
